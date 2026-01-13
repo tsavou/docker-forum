65326fc967ea724d2d5f5cc64f374b23eb96a9b1
tsavou <theosavourat@gmail.com>
2026-01-14 00:24:03 +0100

    refactor: enhance Vite configuration and remove Vitest config file
    - Update Vite configuration to conditionally load Vue DevTools based on the environment.
    - Add test configuration directly within the Vite config.
    - Remove the separate Vitest configuration file as its settings are now integrated into Vite.

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,20 +1,32 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueDevTools from 'vite-plugin-vue-devtools';
+import { configDefaults } from 'vitest/config';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(async ({ mode }) => {
+  const plugins = [vue()];
+
+  // On charge les DevTools SEULEMENT si on n'est pas en test
+  if (mode !== 'test') {
+    const { default: vueDevTools } = await import('vite-plugin-vue-devtools');
+    plugins.push(vueDevTools());
+  }
+
+  return {
+    plugins: plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-
-  server: {
-    host: '0.0.0.0', // Permet d'accepter les connexions externes
-    port: 80,
-  },
+    server: {
+      host: '0.0.0.0',
+      port: 80,
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/**'],
+    },
+  };
 });
